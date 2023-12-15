1e25c43f0b46f024e692184439ec0e94f686d057
Serhii Bogdanov <bohdanov.serhey@gmail.com>
2023-12-15 10:12:55 +0200

    Update ContactList.js

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,18 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact, fetchContacts } from 'redux/operations';
-import { selectContacts, selectStatusFilter } from 'redux/selectors';
+import {  selectVisibleContacts } from 'redux/selectors';
 import { List, ListBtn, ListItem, ListName } from './ContactListStyled';
 import { useEffect } from 'react';
 
 const ContactList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
-  const filtered = useSelector(selectStatusFilter);
+ 
 
-  const normalizedFilter = filtered.toLowerCase();
-  const filteredContacts = contacts.filter(({ name }) =>
-    name.toLowerCase().includes(normalizedFilter)
-  );
+  const filteredContacts =  useSelector(selectVisibleContacts);
 
   useEffect(() => {
     dispatch(fetchContacts());
